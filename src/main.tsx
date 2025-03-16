aba328cbd26408887c0d5fad4b3d1d7e440976b9
Alfonso Manuel Romano <alfonso.manuel.romano@gmail.com>
2025-03-16 23:03:18 +0100

    Login page

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { PrimeReactProvider } from "primereact/api";
+import { PrimeReactProvider } from 'primereact/api';
 import './index.css'
 import App from './App.tsx'
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -27,24 +27,41 @@ export default function Login() {
     };
 
     return (
-        
-        <div className="flex align-items-center justify-content-start">
-            <div className="block bg-primary font-bold text-center p-4 border-round mb-3">
-                <h1>FitVAULT</h1>
-            </div>
-            <div className="block bg-primary font-bold text-center p-4 border-round mb-3">
-                {error && <p className="error">{error}</p>}
-            </div>
-            <div className="block bg-primary font-bold text-center p-4 border-round mb-3">
-                <InputText type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-            </div>
-            <div className="block bg-primary font-bold text-center p-4 border-round mb-3">
-                <InputText type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </div>
-            <div className="block bg-primary font-bold text-center p-4 border-round mb-3">
-                <Button onClick={handleLogin}>Login</Button>
+
+        <div className="surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
+            <div className="flex flex-column align-items-center justify-content-center">
+                <img src="C:\Users\Pr3S7igI0\Downloads\logo-white" alt="logo" className="mb-5 w-6rem flex-shrink-0"/>
+                <div>
+                    <div className="w-full surface-card py-8 px-5 sm:px-8">
+                        <div className="text-center mb-5">
+                            <img src="/demo/images/login/avatar.png" alt="Image" height="50" className="mb-3"/>
+                            <div className="text-900 text-3xl font-medium mb-3">
+                                Welcome!
+                            </div>
+                            <span className="text-600 font-medium">Sign in to continue</span>
+                        </div>
+                    
+                        <div className="block bg-primary font-bold text-center border-round mb-3">
+                            {error && <p className="error">{error}</p>}
+                        </div>
+                        
+                        <div className="font-bold block bg-primary text-center border-round p-1 mb-3 p-float-label">
+                            <i className="pi pi-user" />
+                            <InputText id="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                            <label htmlFor="username">Username</label>
+                        </div>
+
+                        <div className="block bg-primary text-center border-round p-1 mb-3 p-float-label">
+                            <InputText id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                            <label className="font-bold block text-center mb-3" htmlFor="password">Password</label>
+                        </div>
+
+                        <div className="block bg-primary font-bold text-center border-round mb-3">
+                            <Button onClick={handleLogin}>Login</Button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
     );
 }
